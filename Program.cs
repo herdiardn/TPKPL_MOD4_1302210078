70075229b3afee450ffee488d9d07b71fb6f66d8
herdiardn <herdiardan@gmail.com>
2023-03-12 20:19:56 +0700

    “menambahkan implementasi dengan table-driven

--- Program.cs
+++ Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace tpmodul4_1302210078
+namespace tpmod4_1302210078
 {
-    class Program
+    internal class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            int getKodePos = KodePos.getKodePos(KodePos.Kelurahan.Batununggal);
+            Console.WriteLine("Kode pos Batununggal adalah :");
+            Console.WriteLine(getKodePos);
+            Console.WriteLine();
+        }
+    }
+    public class KodePos
+    {
+        public enum Kelurahan { Batununggal, Kujangsari, Mengger, Weters, Cijaura, Jatisari, Margasari, Sekajati, Kebonwaru, Maleer, Samoja }
+        public static int getKodePos(Kelurahan kel)
+        {
+            int[] kodepos = { 40266, 40287, 40256, 40287, 40286, 40272, 40274, 40273 };
+            return kodepos[(int)kel];
         }
     }
 }
