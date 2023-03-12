30455e7b41d06d1ce264eac2b9677e1a1a083014
herdiardn <herdiardan@gmail.com>
2023-03-12 18:08:49 +0700

    inisialisasi projek

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tpmodul4_1302210078
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
